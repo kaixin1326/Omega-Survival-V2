dd4017850bb3589aaa570f387c03c2532fa8ef8b
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-03-07 07:03:28 -0600

    Fix blink

--- Assets/Scripts/BlinkEffect.cs
+++ Assets/Scripts/BlinkEffect.cs
@@ -6,18 +6,28 @@ using UnityEditor;
 public class BlinkEffect : MonoBehaviour
 {
     public float glowMaxSize;
+    public float timeDif = 0.01f;
+    private float lasttime = 0;
+    private bool isTrue = true;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        SerializedObject halo = new SerializedObject(GetComponent("Halo"));
-        halo.FindProperty("m_Size").floatValue = Mathf.PingPong(Time.time, glowMaxSize);
-        halo.ApplyModifiedProperties();
+        //SerializedObject halo = new SerializedObject(GetComponent("Halo"));
+        //halo.FindProperty("m_Size").floatValue = Mathf.PingPong(Time.time, glowMaxSize);
+        //halo.ApplyModifiedProperties();
+        Component halo = GetComponent("Halo");
+        if(Time.time - lasttime >= timeDif)
+        {
+            lasttime = Time.time;
+            halo.GetType().GetProperty("enabled").SetValue(halo, !isTrue);
+            isTrue = !isTrue;
+        }
     }
 }
